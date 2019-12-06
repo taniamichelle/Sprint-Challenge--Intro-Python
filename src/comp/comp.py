373f13ad413673f40c9e80d13bef40fb3447bfdd
tania <tanislavens@yahoo.com>
2019-12-06 15:11:34 -0500

    half of comp file completed

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -20,28 +20,32 @@ humans = [
     Human("Igon", 41),
     Human("David", 31),
 ]
+# print(humans)
+h = Human("Joe", 49)
+print('name of h', h.name)
+print('age of h', h.age)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
 print("Starts with D: ")
-a = [h for h in str(humans) if h[0] == 'D']
+a = [d.name for d in humans if d.name[0] == 'D']
 print(a)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
-b = []
+b = [e.name for e in humans if e.name[-1] == 'e']
 print(b)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
-c = []
+c = [x.name for x in humans if x.name[0] == 'C' or x.name[0] =='D' or x.name[0] =='E' or x.name[0] =='F' or x.name[0] == 'G']
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
-d = []
+d = [a.age + 10 for a in humans]
 print(d)
 
 # Write a list comprehension that creates a list of strings which are the name
@@ -54,14 +58,14 @@ print(e)
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = []
-print(f)
+f = [n.name for n in humans and a.age for a in humans if 27 >= a.age >= 32)]
+print(convert(f))
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = []
+g = [u.name.upper() for u in humans + f.age + 5 for f in humans]
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
