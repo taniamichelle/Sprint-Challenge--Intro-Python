2742b258d63378e90a34b8ab53104077c70a3c13
tania <tanislavens@yahoo.com>
2019-12-06 11:19:28 -0500

    completed oop1 file

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,44 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+class Vehicle:
+    '''
+    this is the Vehicle base (parent) class
+    '''
+    pass
+
+class FlightVehicle(Vehicle):
+    '''
+    this is the FlightVehicle class, subclass of Vehicle
+    '''
+    pass
+
+class Starship(FlightVehicle):
+    '''
+    this is the Starship class, subclass of FlightVehicle
+    '''
+    pass
+
+class Airplane(FlightVehicle):
+    '''
+    this is the Airplane class, subclass of Flightvehicle
+    '''
+    pass
+
+class GroundVehicle(Vehicle):
+    '''
+    this is the GroundVehicle class, subclass of Vehicle
+    '''
+    pass
+
+class Car(GroundVehicle):
+    '''
+    this is the Car class, subclass of GroundVehicle
+    '''
+    pass
+
+class Motorcycle(GroundVehicle):
+    '''
+    this is the Motorcycle class, subclass of GroundVehicle
+    '''
+    pass 
