f0952b9ea1169ae6ec32ef0a890e1fb0a074d4b4
tania <tanislavens@yahoo.com>
2019-12-06 20:42:02 -0500

    completed comp file

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -21,9 +21,6 @@ humans = [
     Human("David", 31),
 ]
 # print(humans)
-h = Human("Joe", 49)
-print('name of h', h.name)
-print('age of h', h.age)
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
@@ -53,27 +50,70 @@ print(d)
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
-e = []
+e = [z.name + '-' + str(z.age) for z in humans]
 print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
-# res = list(map(eval, humans))
-# print('humans tuple: ', res)
-# print("Names and ages between 27 and 32:")
-# f = [t for t in humans if 27 >= t.age >= 32]
+'''
+objective: create a list of tuples containing names and corresponding ages 27-32
+exp tuple: (name(data_type: string), age(data_type: int))
+exp print: list of tuples
+
+example tuple: ("David", 31) 
+example print: [("David", 31), ("David", 31), ("David", 31)]
+
+constraints: ages between 27 and 32, inclusive
+
+1) create a list of tuples using a for loop
+2) create a tuple
+
+'''
+# t = ("David", 31)
+# print('t', t)
+
+# # List comprehension
+# f = [t for item in range(12)] 
 # print(f)
+
+# # for loop
+# f2 = []
+# for item in range(12): # range(start_inclusive, end_exclusive)
+#     print(t)
+#     f2.append(t)
+# print(f2)
+
+ducky = Human("Ducky", 3)
+print('name: ', ducky.name)
+print('age: ', ducky.age)
+
+# t = (ducky.name, ducky.age)
+# f = [t for item in range(12)]
+# print(f)
+
+# list of objects
+# f = [t.name + str(t.age) for t in humans]
+# print(f)
+
+# list of strings
+f = [(human.name, human.age) for human in humans if 27 <= human.age <= 32]
+print("Names and ages between 27 and 32:")
+print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
-# print("All names uppercase:")
-# g = humans.copy([u.name.upper() for u in humans + f.age + 5 for f in humans])
-# print(g)
+
+print("All names uppercase:")
+g = [Human(human.name.upper(), human.age + 5) for human in humans]
+
+    # print(f"<Human: {human.name.upper()}, {str(human.age + 5)}>")
+    # g.append(f"<Human: {human.name.upper()}, {str(human.age + 5)}>")
+print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
 import math
-h = [r.sqrt]
+h = [math.sqrt(human.age) for human in humans]
 print(h)
